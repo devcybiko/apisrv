3d504dcddec17e7c4908d9e21dfd92b363ac7ab3
Greg Smith <thinkable@thinkable.us>
2020-03-24 23:07:07 -0400

    automatic push

--- sample/api/.naiwelogs/uniqueSourceErrors.js
+++
@@ -1,5 +0,0 @@
-const sql = `SELECT DISTINCT srccode FROM rawlogs`;
-const uniqueSourceErrors = function(data, context, callback) {
-    return this.query(sql, data, context, callback);
-}
-module.exports = uniqueSourceErrors;
