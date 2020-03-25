06af5cb6d9d3f1c275418bdad56c06d7cd395c9e
Greg Smith <thinkable@thinkable.us>
2020-03-24 23:40:02 -0400

    automatic commit

--- api/fun.js
+++ api/fun.js
@@ -1,6 +1,9 @@
 let funConstructor = function (config) {
-    this.data = function(req, res, data) {
-        return res.send({"a":"alpha", "b":"beta", "c": "gamma"});
+    this.data = function(req, res, data) { // api/fun/data?arg=0
+        let results = [];
+        results.push(`{"a":"alpha", "b":"beta", "c": "gamma"}`);
+        results.push(`{"superman":"clark kent", "batman":"bruce wayne", "spider-man": "peter parker"}`);
+        return res.send(results[+data.arg]);
     }
 }
 
--- site/static/js/main.js
+++ site/static/js/main.js
@@ -1,3 +1,6 @@
-$.get( "api/fun", function( data ) {
+$.get( "api/fun/data?arg=0", function( data ) {
     alert( data );
+    $.get( "api/fun/data?arg=1", function( data ) {
+        alert( data );
+      });
   });
